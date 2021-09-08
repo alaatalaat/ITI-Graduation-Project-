17bc0b8c9759a90661de962c539d83c65269612b
abdo-salem <abdomsalem895@gmail.com>
2021-09-08 18:07:41 +0200

    login as admin

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,23 +4,21 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  fname:string ="";
-  lname:string ="";
-  erorrmsg:string ="";
-  email:any ="";
-  phone:number=0;
-  constructor(public Routerlnk:Router) { }
-  loginpage(x:any ,y:any){
-    if(x.value != "" && y.value != ""){
-      this.Routerlnk.navigateByUrl("/login")
-    }else{
-      this.erorrmsg= "you must fill all fields"
+  fname: string = '';
+  lname: string = '';
+  erorrmsg: string = '';
+  email: any = '';
+  phone: number = 0;
+  constructor(public Routerlnk: Router) {}
+  loginpage(x: any, y: any) {
+    if (x.value != '' && y.value != '') {
+      this.Routerlnk.navigateByUrl('/home');
+    } else {
+      this.erorrmsg = 'you must fill all fields';
     }
   }
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
